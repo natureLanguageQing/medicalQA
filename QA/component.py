f76e52b2f4f20b93dce005f3a34e49fba8bf9881
yanguangqing <yanguangqing@miao.cn>
2020-01-15 20:56:58 +0800

    补全一下哈哈

--- QA/component.py
+++ QA/component.py
@@ -20,14 +20,10 @@ def get_medical_name(words: str):
     return answer
 
 
-# lc.loc[lc["grade"] == "B"].head()
-var = instructions.loc[instructions["name"] == "同仁堂 锁阳固精丸 9g*10丸"]
-# .str.contains(r'.*?n.*')
-print(var)
-var = instructions.loc[instructions["name"].str.contains(r'同仁堂')]
-print(var)
 # 十味诃子丸的成分是什么
 for i in drug_names.values.tolist():
+    if "(" in i[0] or ")" in i[0]:
+        continue
     question_drug = get_medical_name(i[0]+"的成分是什么")
     # 因为我并没有对话数据集，所以这个部分应该是一个分类模型去决策寻找哪个关系。
     print("下一个问题")
@@ -40,13 +36,12 @@ for i in drug_names.values.tolist():
         print("without_answer：暂无相关数据，已经责令运营快速补充数据，请稍后哟！！")
     elif answers_recall.shape[0] == 1:
         print("answer:", question_drug, "的成分是", answers_recall["chengfen"].values.tolist()[0])
-    else:
-        print("answer:", "名字包含", question_drug, "有", answers_recall.name.values.tolist(), "请问你问的是哪一种？")
-    wait_choose = ""
-    wait_chooses = answers_recall.name.values.tolist()
-    for i in range(len(wait_chooses)):
-        if i != len(wait_chooses)-1:
-            wait_choose += wait_chooses[i] + "、"
-        else:
-            wait_choose += wait_chooses[i] + "。"
-    print("exception_answer:", "名字包含", question_drug, "有", wait_choose, "请问你问的是哪一种？")
+    elif answers_recall.shape[0] > 2:
+        wait_choose = ""
+        wait_chooses = answers_recall.name.values.tolist()
+        for i in range(len(wait_chooses)):
+            if i != len(wait_chooses)-1:
+                wait_choose += wait_chooses[i] + "、"
+            else:
+                wait_choose += wait_chooses[i] + "。"
+        print("exception_answer:", "名字包含", question_drug, "有", wait_choose, "请问你问的是哪一种？")
